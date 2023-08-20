ca4f12785e032aaf63d4d8427034bceced843c10
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-08-20 06:39:54 -0700

    Added

--- lib/all_things_social_web/live/content_board_live/show.ex
+++ lib/all_things_social_web/live/content_board_live/show.ex
@@ -13,11 +13,22 @@ defmodule AllThingsSocialWeb.ContentBoardLive.Show do
     potential_influencer_accounts =
       ContentBoards.list_potential_influencer_accounts_for_a_content_board(id)
 
-    IO.inspect(potential_influencer_accounts)
+    outreached_influencer_accounts =
+      ContentBoards.list_outreached_influencer_accounts_for_a_content_board(id)
+
+    negotiating_influencer_accounts =
+      ContentBoards.list_negotiating_influencer_accounts_for_a_content_board(id)
+
+    active_influencer_accounts =
+      ContentBoards.list_active_influencer_accounts_for_a_content_board(id)
 
     {:noreply,
      socket
      |> assign(:page_title, page_title(socket.assigns.live_action))
+     |> assign(:potential_influencer_accounts, potential_influencer_accounts)
+     |> assign(:outreached_influencer_accounts, outreached_influencer_accounts)
+     |> assign(:negotiating_influencer_accounts, negotiating_influencer_accounts)
+     |> assign(:active_influencer_accounts, active_influencer_accounts)
      |> assign(:content_board, ContentBoards.get_content_board!(id))}
   end
 
